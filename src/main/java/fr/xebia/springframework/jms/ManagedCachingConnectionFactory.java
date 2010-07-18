894427f1e8138bcc70c82da5955e2176c9e5b798
Cyrille Le Clerc <cleclerc@xebia.fr>
2010-07-18 23:40:36 +0000

    Work in progress

---
+++ src/main/java/fr/xebia/springframework/jms/ManagedCachingConnectionFactory.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2008-2009 Xebia and the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fr.xebia.springframework.jms;
+
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectName;
+
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.jms.connection.CachingConnectionFactory;
+import org.springframework.jmx.export.naming.SelfNaming;
+
+/**
+ * <p>
+ * Subclass of {@link CachingConnectionFactory} to expose key attributes and
+ * operations via JMX.
+ * </p>
+ * <p>
+ * Most of the JMX exposition is done via the
+ * {@link ManagedCachingConnectionFactoryMBean}.
+ * </p>
+ * 
+ * @author <a href="mailto:cyrille@cyrilleleclerc.com">Cyrille Le Clerc</a>
+ */
+public class ManagedCachingConnectionFactory extends CachingConnectionFactory implements ManagedCachingConnectionFactoryMBean,
+        BeanNameAware, SelfNaming {
+
+    private String beanName;
+
+    private ObjectName objectName;
+
+    @Override
+    public ObjectName getObjectName() throws MalformedObjectNameException {
+        if (objectName == null) {
+            objectName = ObjectName.getInstance("javax.jms:type=CachingConnectionFactory,name=" + ObjectName.quote(this.beanName));
+        }
+        return objectName;
+    }
+
+    /**
+     * make visible for JMX
+     */
+    @Override
+    public boolean isReconnectOnException() {
+        return super.isReconnectOnException();
+    }
+
+    @Override
+    public void setBeanName(String beanName) {
+        this.beanName = beanName;
+    }
+
+    public void setObjectName(ObjectName objectName) {
+        this.objectName = objectName;
+    }
+}
---
+++ src/main/java/fr/xebia/springframework/jms/ManagedCachingConnectionFactoryMBean.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2008-2009 Xebia and the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fr.xebia.springframework.jms;
+
+import org.springframework.jms.connection.CachingConnectionFactory;
+
+/**
+ * Exposed attributes and operations exposed via JMX by the
+ * {@link ManagedCachingConnectionFactory} and its
+ * {@link CachingConnectionFactory} parent.
+ * 
+ * @author <a href="mailto:cyrille@cyrilleleclerc.com">Cyrille Le Clerc</a>
+ */
+public interface ManagedCachingConnectionFactoryMBean {
+    int getSessionCacheSize();
+
+    boolean isCacheConsumers();
+
+    boolean isCacheProducers();
+
+    boolean isReconnectOnException();
+
+    void resetConnection();
+
+    void setCacheConsumers(boolean cacheConsumers);
+
+    void setCacheProducers(boolean cacheProducers);
+
+    void setReconnectOnException(boolean reconnectOnException);
+
+    void setSessionCacheSize(int sessionCacheSize);
+}
