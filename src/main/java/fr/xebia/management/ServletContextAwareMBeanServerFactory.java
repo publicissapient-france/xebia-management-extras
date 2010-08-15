5acf3c6ba5ad4ce8e52cb96cd7b99f7847e01276
Cyrille Le Clerc <cleclerc@xebia.fr>
2010-08-15 15:15:18 +0000

    Work in progress

--- src/main/java/fr/xebia/management/ServletContextAwareMBeanServerFactory.java
+++ src/main/java/fr/xebia/management/ServletContextAwareMBeanServerFactory.java
@@ -15,12 +15,15 @@
  */
 package fr.xebia.management;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Hashtable;
+import java.util.Map;
 
 import javax.management.MBeanServer;
 import javax.management.MalformedObjectNameException;
@@ -38,49 +41,71 @@ import org.springframework.web.context.ServletContextAware;
 
 /**
  * <p>
- * Instantiate {@link MBeanServer} which add a "path" property with value
- * {@link ServletContext#getContextPath()} to each {@link ObjectName} passed as
- * method parameter. The goal is to prevent collisions between MBeans declared
- * in different web applications.
+ * Instantiate {@link MBeanServer} which add a "path" property with value {@link ServletContext#getContextPath()} to each {@link ObjectName}
+ * passed as method parameter. The goal is to prevent collisions between MBeans declared in different web applications.
  * </p>
  * <p>
- * Sample : EHCache's {@link net.sf.ehcache.management.ManagementService} will
- * register Hibernate's {@linkplain org.hibernate.cache.StandardQueryCache} as
- * 
- * <code>net.sf.ehcache:CacheManager=my-cachemanager,name=org.hibernate.cache.StandardQueryCache,type=CacheStatistics</code>
- * that could collide with other applications and this MBeanServer will add the
- * <code>path</code> attribute to prevent problems :
- * 
+ * Sample : EHCache's {@link net.sf.ehcache.management.ManagementService} will register Hibernate's
+ * {@linkplain org.hibernate.cache.StandardQueryCache} as
+ * <code>net.sf.ehcache:CacheManager=my-cachemanager,name=org.hibernate.cache.StandardQueryCache,type=CacheStatistics</code> that could
+ * collide with other applications and this MBeanServer will add the <code>path</code> attribute to prevent problems :
  * <code>net.sf.ehcache:CacheManager=my-cachemanager,name=org.hibernate.cache.StandardQueryCache,type=CacheStatistics,path=/my-application</code>
  * .
  * </p>
  * <p>
- * The added property was named '<code>path</code>' to follow Tomcat JMX beans
- * naming convention.
+ * The added property was named <code>path</code> to follow Tomcat JMX beans naming convention.
+ * </p>
+ * <p>
+ * If the underlying sertvlet container is Tomcat, another extra property named <code>host</code> with the name of the Tomcat host in which
+ * the webapp is declared is added.
+ * </p>
  * <p>
  * This {@link FactoryBean} doesn't extend {@link AbstractFactoryBean} due to <a
- * href="http://jira.springframework.org/browse/SPR-4968">SPR-4968 : Error
- * "Singleton instance not initialized yet" triggered by toString call in case
- * of circular references</a>
+ * href="http://jira.springframework.org/browse/SPR-4968">SPR-4968 : Error "Singleton instance not initialized yet" triggered by toString
+ * call in case of circular references</a>
  * </p>
  * 
- * @author <a href="mailto:cyrille@cyrilleleclerc.com">Cyrille Le Clerc</a>
+ * @author <a href="mailto:cyrille.leclerc@pobox.com">Cyrille Le Clerc</a>
  */
 public class ServletContextAwareMBeanServerFactory implements FactoryBean<MBeanServer>, ServletContextAware, InitializingBean {
-
+    
     private final static Logger logger = LoggerFactory.getLogger(ServletContextAwareMBeanServerFactory.class);
-
+    
     protected MBeanServer instance;
-
+    
     protected MBeanServer mbeanServer;
-
+    
     protected ServletContext servletContext;
-
+    
+    protected Map<String, String> objectNameExtraAttributes = new HashMap<String, String>();
+    
     public void afterPropertiesSet() throws Exception {
         Assert.notNull(this.mbeanServer, "mbeanServer can NOT be null");
         Assert.notNull(this.servletContext, "servletContext can NOT be null");
+        
+        objectNameExtraAttributes.put("path", servletContext.getContextPath());
+        
+        if ("org.apache.catalina.core.ApplicationContextFacade".equals(servletContext.getClass().getName())) {
+            
+            Field applicationContextFacadeContextField = Class.forName("org.apache.catalina.core.ApplicationContextFacade")
+                .getDeclaredField("context");
+            applicationContextFacadeContextField.setAccessible(true);
+            
+            Field applicationContextContextField = Class.forName("org.apache.catalina.core.ApplicationContext").getDeclaredField("context");
+            applicationContextContextField.setAccessible(true);
+            
+            Field standardContextHostNameField = Class.forName("org.apache.catalina.core.StandardContext").getDeclaredField("hostName");
+            standardContextHostNameField.setAccessible(true);
+            
+            Object applicationContext = applicationContextFacadeContextField.get(servletContext);
+            Object standardContext = applicationContextContextField.get(applicationContext);
+            String hostName = (String)standardContextHostNameField.get(standardContext);
+            
+            objectNameExtraAttributes.put("host", hostName);
+        }
+        logger.trace("Extra objectname attributes : {}" + this.objectNameExtraAttributes);
     }
-
+    
     public MBeanServer getObject() throws Exception {
         if (instance == null) {
             InvocationHandler invocationHandler = new InvocationHandler() {
@@ -89,8 +114,8 @@ public class ServletContextAwareMBeanServerFactory implements FactoryBean<MBeanS
                     for (int i = 0; i < modifiedArgs.length; i++) {
                         Object arg = modifiedArgs[i];
                         if (arg instanceof ObjectName) {
-                            ObjectName objectName = (ObjectName) arg;
-                            modifiedArgs[i] = addPathPropertyToObjectName(objectName);
+                            ObjectName objectName = (ObjectName)arg;
+                            modifiedArgs[i] = addExtraAttributesToObjectName(objectName);
                         }
                     }
                     if (logger.isDebugEnabled()) {
@@ -102,43 +127,42 @@ public class ServletContextAwareMBeanServerFactory implements FactoryBean<MBeanS
                         throw ite.getCause();
                     }
                 }
-
+                
                 /**
                  * <p>
-                 * Copy the given <code>objectName</code> adding a "path"
-                 * property with value {@link ServletContext#getContextPath()}
+                 * Copy the given <code>objectName</code> adding the extra attributes.
                  * </p>
                  */
-                protected ObjectName addPathPropertyToObjectName(ObjectName objectName) throws MalformedObjectNameException {
+                protected ObjectName addExtraAttributesToObjectName(ObjectName objectName) throws MalformedObjectNameException {
                     Hashtable<String, String> table = new Hashtable<String, String>(objectName.getKeyPropertyList());
-                    table.put("path", servletContext.getContextPath());
-
+                    table.putAll(objectNameExtraAttributes);
+                    
                     ObjectName result = ObjectName.getInstance(objectName.getDomain(), table);
-                    if (logger.isTraceEnabled()) {
-                        logger.trace("addPathPropertyToObjectName(objectName=" + objectName + "):" + result);
-                    }
+                    logger.trace("addExtraAttributesToObjectName({}): {}", objectName, result);
+                    
                     return result;
                 }
             };
-            instance = (MBeanServer) Proxy.newProxyInstance(ClassUtils.getDefaultClassLoader(), new Class[] { MBeanServer.class },
-                    invocationHandler);
-
+            instance = (MBeanServer)Proxy.newProxyInstance(ClassUtils.getDefaultClassLoader(), new Class[] {
+                MBeanServer.class
+            }, invocationHandler);
+            
         }
         return instance;
     }
-
+    
     public Class<? extends MBeanServer> getObjectType() {
         return MBeanServer.class;
     }
-
+    
     public boolean isSingleton() {
         return true;
     }
-
+    
     public void setMbeanServer(MBeanServer mbeanServer) {
         this.mbeanServer = mbeanServer;
     }
-
+    
     public void setServletContext(ServletContext servletContext) {
         this.servletContext = servletContext;
     }
